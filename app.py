d0aaa8e29bc5a3ff6d03e1229d647308c456ee02
Stefano Esposito <stefanoesposito75@gmail.com>
2024-11-15 17:02:34 +0100

    sitemap and robots

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -47,6 +47,51 @@ def it_page(page):
 @app.route('/en/<page>')
 def en_page(page):
     return render_template(f'en/{page}.html')
+
+@app.route('/sitemap.xml', methods=['GET'])
+def sitemap():
+    pages = []
+    with app.test_request_context():
+        # Aggiungi solo le pagine esistenti
+        static_pages = [
+            url_for('home', _external=True),
+            url_for('it_home', _external=True),
+            url_for('en_home', _external=True)
+        ]
+        pages.extend(static_pages)
+
+        # Aggiungi le pagine dinamiche esistenti
+        dynamic_pages = [
+            url_for('it_page', page='contact', _external=True),
+            url_for('it_page', page='portfolio', _external=True),
+            url_for('it_page', page='su-di-me', _external=True),
+            url_for('en_page', page='contact', _external=True),
+            url_for('en_page', page='about-me', _external=True),
+            url_for('en_page', page='portfolio', _external=True)
+        ]
+        pages.extend(dynamic_pages)
+
+    sitemap_xml = ['<?xml version="1.0" encoding="UTF-8"?>']
+    sitemap_xml.append('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
+
+    for page in pages:
+        sitemap_xml.append(f"<url><loc>{page}</loc></url>")
+
+    sitemap_xml.append('</urlset>')
+    sitemap_xml = "\n".join(sitemap_xml)
+
+    return Response(sitemap_xml, mimetype='application/xml')
+
+
+# Robots.txt
+@app.route('/robots.txt', methods=['GET'])
+def robots():
+    robots_txt = f"""
+    User-agent: *
+    Allow: /
+    Sitemap: {url_for('sitemap', _external=True)}
+    """
+    return Response(robots_txt, mimetype='text/plain')
 
 if __name__ == '__main__':
     app.run(debug=True)
